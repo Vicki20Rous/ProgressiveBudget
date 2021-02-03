4bf22726e9d26ae20542b5c99f343db7a37f36e1
Victoria Rouser <vic86rous@gmail.com>
2021-02-03 04:39:20 -0600

    updated

---
+++ public/db.js
@@ -0,0 +1,57 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+  db = event.target.result;
+  console.log("Web app request to use IndexDB successful.");
+  
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(event) {
+    console.error("The following error has been found: " + "\n" + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+  console.log("Records have been stored.")
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+
+        store.clear();
+        console.log("Stored records have been cleared.")
+      });
+    }
+  };
+}
+
+window.addEventListener("online", checkDatabase);
---
+++ public/server-worker.js
@@ -0,0 +1,68 @@
+// Declare an array of FILES_TO_CACHE inside an array of strings
+const FILES_TO_CACHE = ["/", "db.js", "/index.html", "index.js", "styles.css"];
+
+// Declare CACHE_NAME as static-cache-v2
+const CACHE_NAME = "static-cache-v2";
+
+// Declare DATA_CACHE_NAME as data-cache-v1
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// Create self addEventListener() to install with a function to wait until files have been pre-cached by opening CACHE_NAME as an argument and adding all FILES_TO_CACHE as a return
+self.addEventListener("install", function(event) {
+    
+    // Event to pre-cache files
+    event.waitUntil(
+        caches.open(CACHE_NAME) // If code doesn't work, join ".then..." line here
+        .then(cache => {
+            console.log(`Files pre-cached successfully.`);
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    // Added self.skipWaiting()
+    self.skipWaiting();
+})
+
+
+// Create self addEventListener() to activate with a function and map keys
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Previous cache data removed", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    
+    // Added self.clientes.claim()
+    self.clients.claim();
+});
+
+// Create self addEventListener() to fetch api to clone and store response in cache
+self.addEventListener("fetch", function(event) {
+    if (event.request.url.includes("/api/")) {
+      event.respondWith((
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      ));
+  
+      return;
+  }});
